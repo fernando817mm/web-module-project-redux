d0685a0561964c3d0f59425a5b5f6187c277e465
Fernando Martinez <fernando817mm@gmail.com>
2021-08-10 17:04:04 -0500

    mvp completed

---
+++ src/actions/favMovieActions.js
@@ -0,0 +1,13 @@
+export const TOGGLE_FAVORITES = 'TOGGLE_FAVORITES';
+export const ADD_FAVORITES = 'ADD_FAVORITES';
+export const REMOVE_FAVORITES = 'REMOVE_FAVORITES';
+
+export const toggleFavorites = () => {
+    return ({type: TOGGLE_FAVORITES});
+}
+export const addFavorite = (movie) => {
+    return ({type: ADD_FAVORITES, payload: movie});
+}
+export const removeFavorite = (id) => {
+    return ({type: REMOVE_FAVORITES, payload: id});
+}
